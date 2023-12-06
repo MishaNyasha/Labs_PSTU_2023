eeb39be0bcd80a2e9ac82f61a0708cfe5333fd48
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-06 12:18:01 +0500

    Add files via upload

---
+++ Sem_1/2_7 /A/2_7A.cpp
@@ -0,0 +1,27 @@
+﻿﻿#include <iostream>
+#include <cmath>
+#include <locale.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	int r = sqrt(n);
+	if (r * r == n)
+	{
+		for (int i = 1; i <= r; i++)
+		{
+			for (int j = 1; j <= r; j++)
+			{
+				cout << " *";
+			}
+			cout << endl;
+		}
+	}
+	else
+	{
+		setlocale(LC_ALL, "Russian");
+		cout << "число не соответсвует требованиям" << endl;
+	}
+	return 0;
+}
