12b8d2642e09a7861c9f0fedb405fb9b48bd1240
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-06 12:03:07 +0500

    Add files via upload

---
+++ Sem_1/2_5/2_5.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int N;
+	cin >> N;
+	int star = 1;
+	int space = N - 1;
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 0; j < space; j++)
+		{
+			cout << "  ";
+		}
+		space--;
+		for (int j = 0; j < star; j++)
+		{
+			cout << " *";
+		}
+		star += 1;
+		cout << endl;
+	}
+
+	return 0;
+}
