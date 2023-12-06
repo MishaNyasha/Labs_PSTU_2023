dda7a1edb42932b1e51fde66029546647b24d904
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-06 12:03:26 +0500

    Add files via upload

---
+++ Sem_1/2_6/2_6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) 
+    {
+        for (int j = 0; j < i; j++) 
+        {
+            cout << "  ";
+        }
+        for (int k = i; k < N; k++) 
+        {
+            cout << " *";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
