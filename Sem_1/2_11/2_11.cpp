ad9fc43b9b925e737fbbb8148afc4e2387020064
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-06 12:20:54 +0500

    Add files via upload

---
+++ Sem_1/2_11/2_11.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, tmp;
+    bool plusFirst = false;
+    bool minusFirst = false;
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите длину последовательности:" << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> tmp;
+        if (!plusFirst && !minusFirst)
+        {
+            if (tmp > 0) 
+            {
+                plusFirst = true; 
+            }
+            else 
+                if (tmp < 0) 
+                {
+                     minusFirst = true;
+                }
+        }
+    }
+    if (plusFirst)
+    {
+        cout << "Сначала ввели положительное число";
+    }
+    else 
+        if (minusFirst)
+        {
+            cout << "Сначала ввели отрицательное число";
+        }
+        else
+        {
+            cout << "Все были нулевыми";
+        }
+         cout << endl;
+}
