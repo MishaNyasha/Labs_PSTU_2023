06d13894967deedd06614faea00292113a4f8c64
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-06 11:29:28 +0500

    Add files via upload

---
+++ Sem_1/1_2/1_2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+    float a, b;
+    cin >> a >> b;
+    if (b == 0)
+    {
+        cout << "Nelzya" << endl;
+    }
+    else
+    {
+        cout << a / b << endl;
+    }
+}
