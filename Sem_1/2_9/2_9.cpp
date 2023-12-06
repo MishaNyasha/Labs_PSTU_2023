0e0cc62b38c4cc260b2cb8ed258cd48645649e71
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-06 12:19:52 +0500

    Add files via upload

---
+++ Sem_1/2_9/2_9.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n / 2; i++)
+	{
+		cout << "  ";
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "* ";
+	}
+	cout << endl;
+	for (int i = 1; i <= n / 2 - 1; i++)
+	{
+		for (int j = n / 2 - 1; j >= i; j--)
+		{
+			cout << "  ";
+		}
+		cout << "* ";
+		for (int j = 1; j <= n - 2; j++)
+		{
+			cout << "  ";
+		}
+		cout << "* ";
+		for (int j = i; j > 1; j--)
+		{
+			cout << "  ";
+		}
+		cout << "* \n";
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "* ";
+	}
+	for (int i = 1; i <= (n / 2) - 1; i++)
+	{
+		cout << "  ";
+	}
+	cout << "*  \n";
+	for (int i = 1; i <= (n - 1) / 2; i++)
+	{
+		cout << "* ";
+		for (int j = 1; j <= n - 2; j++)
+		{
+			cout << "  ";
+		}
+		cout << "* ";
+		for (int j = 1; j <= n / 2 - 1; j++)
+		{
+			cout << "  ";
+		}
+		cout << "* \n";
+	}
+	for (int i = 1; i < n / 2; i++)
+	{
+		cout << "* ";
+		for (int j = 1; j <= n - 2; j++)
+		{
+			cout << "  ";
+		}
+		cout << "* ";
+		for (int j = i; j < n / 2 - 1; j++)
+		{
+			cout << "  ";
+		}
+		cout << "* \n";
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		cout << "* ";
+	}
+	return 0;
+}
