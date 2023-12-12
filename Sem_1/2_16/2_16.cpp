830e433f831a3acc2fc34bf067629badc9bbd8f9
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-12 22:15:14 +0500

    Add files via upload

---
+++ Sem_1/2_16/2_16.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+    int N;
+    cin >> N;
+    int Sum = 0;
+    int znak = -1; 
+
+    for (int i = 1; i <= N; i++) 
+    {
+        Sum += znak * i;
+        znak *= -1; 
+    }
+
+    cout << Sum;
+}
