05d0b85d474623b3991839db29211bb16c33b638
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-12 22:15:45 +0500

    Add files via upload

---
+++ Sem_1/2_17/2_17.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    float x, fact, sum;
+
+    cin >> n >> x;
+
+    sum = 1 + x;
+    fact = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        fact *= i;
+        sum += (pow(x, i) / fact);
+    }
+    cout << sum << endl;
+}
