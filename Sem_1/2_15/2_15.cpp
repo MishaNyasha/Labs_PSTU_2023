88755211ff10d57066ddc5569f5cb737a90a88aa
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-12 22:14:49 +0500

    Add files via upload

---
+++ Sem_1/2_15/2_15.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, s, k;
+	bool p = false;
+	setlocale(LC_ALL, "Russian");
+	cout << "введите число" << endl;
+	cin >> n;
+	cout << "введите цифру, которую надо проверить на наличие в числе" << endl;
+	cin >> s;
+	while (n > 0 && !p)
+	{
+		k = n % 10;
+		if (k == s)
+		{
+			p = true;
+			cout << "цифра есть в числе" << endl;
+		}
+		else
+		{
+			n /= 10;
+		}
+	}
+	if (!p)
+	{
+		cout << "цифры нету в числе";
+	}
+}
