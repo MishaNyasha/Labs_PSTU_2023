5acc35b8874971e87b73fd8f31106858093fe606
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-12 22:11:56 +0500

    Add files via upload

---
+++ Sem_1/2_12/2_12.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, tmp, max, min;
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите длину последовательности:" << endl;
+    cin >> n;
+    cout << "Введите первое число:" << endl;
+    cin >> tmp;
+    max = tmp;
+    min = tmp;
+    for (int i = 2; i <= n; i++)
+    {
+        cout << "Введите следующее число:" << endl;
+        cin >> tmp;
+        if (tmp > max)
+        {
+            max = tmp;
+        }
+        else if (tmp < min)
+        {
+            min = tmp;
+        }
+    }
+    cout << "Сумма максимального и минимального элементов: " << (max + min) << endl;
+}
