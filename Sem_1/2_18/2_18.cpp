7b856947084fa02a477b1f1185a37b05cafc0a36
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-12 22:16:12 +0500

    Add files via upload

---
+++ Sem_1/2_18/2_18.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int m = 0;
+    cin >> n;
+    while (n)
+    {
+        m =n % 10;
+        n /= 10;
+    }
+    cout << m << endl;
+}
