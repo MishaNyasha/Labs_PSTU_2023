207ff95a39bb3aa65ed53381cc738308b7c8f44f
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2023-12-16 21:45:55 +0500

    Add files via upload

---
+++ Sem_1/Pointer/4/4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n, tmp, * ptr = &tmp, max, min;
+	cin >> n;
+	cin >> *ptr;
+	max = *ptr;
+	min = *ptr;
+	for (int i = 2, i <= n, i++)
+	{
+		cin >> *ptr;
+		if (*ptr > max)
+		{
+			max = *ptr;
+		}
+		else if (*ptr < min)
+		{
+			min = *ptr;
+		}
+	}
+	cout << "max=" << max << endl;
+	cout << "min=" << min << endl;
+}
