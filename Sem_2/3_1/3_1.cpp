dd487223850d534e43e3a0b76daebcb54988f71b
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-02-04 12:54:13 +0500

    Add files via upload

---
+++ Sem_2/3_1/3_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream> 
+using namespace std;
+int main()
+{
+    int lb = 20, ub = 100;
+    int arr[50];
+    for (int i = 0; i < 50; i++)
+    {
+        arr[i] = (rand() % (ub - lb + 1)) + lb;
+        cout << arr[i] << "  ";
+    }
+    cout << endl << endl << endl;
+    for (int i = 0; i < 50; i++)
+    {
+        cout << arr[49 - i] << "  ";
+    }
+    return 0;
+}
