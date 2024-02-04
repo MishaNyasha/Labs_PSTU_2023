e995d9814cbb5f8c56241bbb2cca5770ef8821b0
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-02-04 12:54:42 +0500

    Add files via upload

---
+++ Sem_2/3_3/3_3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    const int n = 5;
+    bool flag = true;
+    int a[n] = { 1, 2, 3, 4, 5 };
+    for (int i = n - 1; i > 0; i--)
+    {
+        if (a[i] < a[i - 1]) { flag = false; }
+    }
+    if (flag)
+    {
+        cout << "Массив упорядочен" << endl;
+    }
+    else
+    {
+        cout << "Массив не упорядочен" << endl;
+    }
+    return 0;
+}
