bdf489430361c3668d45e3d257d85f94e14eb033
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-02-04 12:54:29 +0500

    Add files via upload

---
+++ Sem_2/3_2/3_2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, tmp, p, q;
+
+    n = 11;
+    p = 2;
+    q = 6;
+
+    int arr[100] = { 41, 42, 66, 87, 86, 38, 77, 84, 45, 35, 22 };
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    while (p < q)
+    {
+        tmp = arr[p];
+        arr[p] = arr[q];
+        arr[q] = tmp;
+        p++;
+        q--;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
