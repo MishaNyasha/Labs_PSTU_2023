f68749e982d94b73685d3d0087f370f3caf28b93
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-02-04 12:55:23 +0500

    Add files via upload

---
+++ Sem_2/3_6/3_6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    const int n = 100;
+    int a[n], max = a[0], maxes = 1;
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 101 - 50;
+        cout << a[i] << "  ";
+    }
+    cout << endl;
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+        else if (a[i] == max)
+        {
+            maxes++;
+        }
+    }
+    cout << "max = " << max << endl<< "maxes = " << maxes;
+    return 0;
+}
