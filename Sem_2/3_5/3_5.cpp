c13c63101284c719b2c4efc9fd809dd501435025
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-02-04 12:55:09 +0500

    Add files via upload

---
+++ Sem_2/3_5/3_5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	const int n = 10;
+	int max, a[n];
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() % 101 - 50;
+		if (a[i] > a[i - 1])
+		{
+			max = a[i];
+		}
+		cout << a[i] << "  ";
+	}
+	cout << endl << max;
+	return 0;
+}
