0a654394ab7f4e2324bd4cb2b46c0508f0905747
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-02-04 12:55:42 +0500

    Add files via upload

---
+++ Sem_2/3_7/3_7.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    const int n = 100;
+    int a[n];
+    int max, min;
+    cout << endl;
+    max = 0;
+    min = 0;
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 101 - 50;
+        if (a[i] > max) 
+        { 
+            max = a[i]; 
+        }
+        if (a[i] < min)
+        {
+            min = a[i];
+        }
+        cout << a[i] << "  ";
+    }
+    cout << endl << "max = " << max << endl << "min = " << min;
+    return 0;
+}
