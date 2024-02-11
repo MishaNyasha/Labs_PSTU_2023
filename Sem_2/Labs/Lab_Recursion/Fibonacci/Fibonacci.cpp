d098e43afd738f8c3a5a41093fb49f32b172a0ab
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-02-11 20:44:08 +0500

    Add files via upload

---
+++ Sem_2/Labs/Lab_Recursion/Fibonacci/Fibonacci.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int foo(int n)
+{
+	if (n <= 1)
+	{
+		return n;
+	}
+	return foo(n - 1) + foo(n - 2);
+}
+int main()
+{
+	int m;
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		cout << foo(i) << " ";
+	}
+	return 0;
+}
