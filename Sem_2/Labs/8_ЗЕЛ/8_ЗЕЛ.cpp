d2842b9b12677665ad2b4bddc5d6cae7ea2102a1
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-02-25 21:24:30 +0500

    Add files via upload

---
+++ Sem_2/Labs/8_ЗЕЛ/8_ЗЕЛ.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+struct state
+{
+    string language;
+    string money;
+    double rate;
+};
+
+using States = map<string, state>;
+
+
+void write_file(const char * file_name, const States& states)
+{
+    
+    ofstream outfile(file_name);
+    if (!outfile.is_open())
+    {
+        cout << "error";
+        exit(0);
+    }
+    for(auto i : states)
+    {
+        outfile << i.first << "  " << i.second.language << "  " << i.second.money << "  " << i.second.rate << endl;
+    }
+    outfile.close();
+}
+
+States read_file(const char * file_name)
+{
+    
+    ifstream infile(file_name);
+    States states;
+    if (!infile.is_open())
+    {
+        cout << "error";
+        exit(0);
+    }
+    while(!infile.eof())
+    {
+        string name, language, money;
+        double rate;
+        infile >> name >> language >> money >> rate;
+        if (name.length()  == 0)
+        {
+            break;
+        }
+        pair<string, state> new_state = pair<string, state>(name, {language, money, rate});
+        states.insert(new_state);
+    }
+    infile.close();
+    return states;
+}
+
+void print_states(const States& states)
+{
+    for(auto i : states)
+    {
+        cout << i.first << "  " << i.second.language << "  " << i.second.money << "  " << i.second.rate << endl;
+    }
+}
+
+
+pair<string, state> read_state_from_user()
+{
+    string name, language, money;
+    double rate;
+    cin >> name >> language >> money >> rate;
+    return pair<string, state>(name, {language, money, rate});
+}
+
+States read_states_from_user()
+{
+    cout << "Введите данные стран (Название, название языка, название денежной единицы, денежная единица относительно $)\n";
+    States states;
+    for(int i = 0; i < 5; i++)
+    {
+        pair<string, state> new_state = read_state_from_user();
+        states.insert(new_state);
+    }
+    return states;
+}
+
+void create_file()
+{
+    States states = read_states_from_user();
+    cout << "Введенные данные\n";
+    print_states(states);
+    write_file("F1", states);
+}
+
+
+void erase_state(States& states)
+{
+    cout << "Введите страну для удаления: ";
+    string state_to_erase;
+    cin >> state_to_erase;
+    states.erase(state_to_erase);
+}
+
+void add_state(States& states)
+{
+    cout << "Введите данные новой страны:\n";
+    pair<string, state> new_state = read_state_from_user();
+    if(states.find(new_state.first) != states.end())
+    {
+        cout << "Страна с данным именем уже существует" << endl;
+        exit(0);
+    }
+    states.insert(new_state);
+}
+
+void process_file()
+{
+    States states = read_file("F1");
+    erase_state(states);
+    add_state(states);
+    add_state(states);
+    cout << "Результат:\n";
+    print_states(states);
+    write_file("F2", states);
+}
+
+int main()
+{
+    create_file();
+    process_file();
+    return 0;
+}
+
+/*
+    Test
+    
+Введите данные стран (Название, название языка, название денежной единицы, денежная единица относительно $)
+Germ
+Germ
+Gr
+0.3939
+Russ
+Russ
+Rub
+0.107107
+Engl
+Engl
+En
+1.07
+Japa
+Japa
+Jp
+0.0032
+Turk
+Turk
+Tr
+0.315
+Введенные данные
+Engl  Engl  En  1.07
+Germ  Germ  Gr  0.3939
+Japa  Japa  Jp  0.0032
+Russ  Russ  Rub  0.107107
+Turk  Turk  Tr  0.315
+Введите страну для удаления: Germ
+Введите данные новой страны:
+Span
+Span
+Sp  
+1.5
+Введите данные новой страны:
+USA
+Amer
+Dollar
+1
+Результат:
+Engl  Engl  En  1.07
+Japa  Japa  Jp  0.0032
+Russ  Russ  Rub  0.107107
+Span  Span  Sp  1.5
+Turk  Turk  Tr  0.315
+USA  Amer  Dollar  1
+*/
