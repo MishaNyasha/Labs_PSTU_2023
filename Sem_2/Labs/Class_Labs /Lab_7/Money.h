4a9d2947c413873ae532155c4e50d440a0b6cda0
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-04-21 15:01:54 +0500

    Create Money.h

---
+++ Sem_2/Labs/Class_Labs /Lab_7/Money.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Money
+{
+public:
+	Money(void);
+	Money(long, int);
+	Money(const Money& a);
+	Money& operator=(const Money&);
+	friend ostream& operator<< (ostream& out, const Money&);
+	friend istream& operator>> (istream& in, Money&);
+	Money operator*(const Money& m);
+	virtual ~Money(void) {};
+private:
+	long rub;
+	int kop;
+};
