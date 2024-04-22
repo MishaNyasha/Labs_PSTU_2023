c311689405942870dba42500862a8434f4dba9df
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-04-22 16:01:32 +0500

    Create Time.h

---
+++ Sem_2/Labs/Class_Labs /Lab_10/Time.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Time
+{
+public:
+    Time();
+    Time(int, int);
+    Time(const Time&);
+    Time operator =(const Time&);
+    bool operator==(const Time&);
+    friend ostream& operator <<(ostream& out, const Time& p);
+    friend istream& operator>>(istream& in, Time& p);
+    friend fstream& operator>>(fstream& fin, Time& p);
+    friend fstream& operator <<(fstream& fout, const Time& p);
+public:
+    ~Time();
+private:
+    int sec;
+    int min;
+};
