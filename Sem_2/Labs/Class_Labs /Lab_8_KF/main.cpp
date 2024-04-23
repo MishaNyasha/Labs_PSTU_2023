cc6330343d6158e96e7a85c3d2f4a1c44b6529ac
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-04-23 13:05:46 +0500

    Create main.cpp

---
+++ Sem_2/Labs/Class_Labs /Lab_8_KF/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "dialog.h"
+#include "event.h"
+#include "vector.h"
+#include "object.h"
+int main()
+{
+	system("chcp 1251 > null");
+	Dialog D;
+	D.Execute();
+	return 0;
+}
