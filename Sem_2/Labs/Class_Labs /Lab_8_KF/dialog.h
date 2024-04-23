e77b7888f5f3cc528f2688f2c71c00de1dc97984
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-04-23 13:04:32 +0500

    Create dialog.h

---
+++ Sem_2/Labs/Class_Labs /Lab_8_KF/dialog.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "event.h"
+#include "vector.h"
+class Dialog : public Vector
+{
+public:
+	Dialog();
+	virtual ~Dialog();
+	virtual void GetEvent(TEvent& event);
+	virtual int Execute();
+	virtual void HandleEvent(TEvent& event);
+	virtual void ClearEvent(TEvent& event);
+	int Valid();
+	void EndExec();
+protected:
+	int EndState;
+};
