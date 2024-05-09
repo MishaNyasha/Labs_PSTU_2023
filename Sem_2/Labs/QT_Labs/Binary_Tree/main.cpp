d0cd38a4973413b6b5d8ca2e28959df477116bad
MishaNyasha <146915883+MishaNyasha@users.noreply.github.com>
2024-05-09 14:58:02 +0500

    Update main.cpp

--- Sem_2/Labs/QT_Labs/Binary_Tree/main.cpp
+++ Sem_2/Labs/QT_Labs/Binary_Tree/main.cpp
@@ -1,11 +1,4 @@
-#include <QApplication>
-#include <QWidget>
-#include <QPainter>
-#include <QFont>
-#include <iostream>
 #include "mainwindow.h"
-#include <vector>
-#include <algorithm>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
